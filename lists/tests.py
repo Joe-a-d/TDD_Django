4eb79e2590f9917c8b5c249a827e1bdda9c82849
Joe-a-d <2334590D@student.gla.ac.uk>
2020-05-17 05:36:33 +0100

    [Recoup LocalSnap] (1) Dispatcher: OK (2) Response : OK

--- lists/tests.py
+++ lists/tests.py
@@ -6,13 +6,27 @@
 
 from django.test import TestCase
 from django.urls import resolve
+from django.http import HttpRequest, HttpResponse
+
 from lists.views import home_page 
+
 
 class HomePageTest(TestCase):
 
     def test_home_resolver(self):
         # resolve() returns a ResolverMatch object if matched, func returns the function to be called
-        # hence the test is defining the following API: GET http://localhost:<PORT#>/ <-> home_page() 
+        # hence the test is defining the following API: GET <BaseURL>/ <-> home_page() 
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_response(self):
+        # test that home_page() returns the appropriate HTML content
+
+        request = HttpRequest()
+        response = home_page(request)
+        content = response.content.decode("utf8")
+
+        self.assertTrue(content.startswith('<html>'))
+        self.assertIn('<h1> Test </h1>', content)
+        self.assertTrue(content.endswith('</html>'))
 
