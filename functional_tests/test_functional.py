05cec2a0777e8feaa2ef3e8947a64375221638d7
Joe-a-d <2334590D@student.gla.ac.uk>
2020-05-21 03:52:11 +0100

    Ch6 ; live_server_url breaks selenium?

--- functional_tests/test_functional.py
+++ functional_tests/test_functional.py
@@ -1,9 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from django.test import LiveServerTestCase
 import time
-import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
+
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
@@ -11,9 +12,17 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+
+	# helpers
+
+	def check_li_created(self, item_text):
+		list = self.browser.find_element_by_id('list_general')
+		list_items = list.find_elements_by_tag_name("li")
+		self.assertIn(item_text, [item.text for item in list_items])
+
 	def test_create_and_get_list(self):
 		# Mike access the homepage
-		self.browser.get("http://localhost:8000")
+		self.browser.get("http://127.0.0.1:8000")
 
 		# He notices that the tab and the page's header mention to-do lists
 		self.assertIn("To-Do", self.browser.title)
@@ -25,22 +34,15 @@ class NewVisitorTest(unittest.TestCase):
 		self.assertEqual(inputbox.get_attribute('placeholder'), 'Add a task')
 
 		# He types 'meditate for 20min' into a textbox
-		text_input = inputbox.send_keys('meditate for 20min')
+		item_text = 'meditate for 20min'
+		text_input = inputbox.send_keys(item_text)
 
 		# When he hits enter the form is sent and the page updates showing his task in an ordered list
 		inputbox.send_keys(Keys.ENTER)
 		time.sleep(1)
 
-		list = self.browser.find_element_by_id('list_general')
-		list_items = list.find_elements_by_tag_name("li")
-		self.assertIn("meditate for 20min", [item.text for item in list_items])
-
+		self.check_li_created(item_text)
+		
 		# The inputbox is still there. He adds another item which reads "clean gutters"
 
 		# The page updates again showing the two items
-
-		self.fail("END")
-
-if __name__ == "__main__":
-	unittest.main()
-
